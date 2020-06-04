f4a03a31e51caa4f5712fb975572a23602f04904
Rajat <rajat.garg@compunneldigital.com>
2020-06-04 18:12:53 +0530

    Day3, day4

---
+++ Day 03 Two City Scheduling.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        FirstCity = [i for i,j in costs]
+        Diff = [j - i for i,j in costs]
+        return sum(FirstCity) + sum(sorted(Diff)[:len(costs)//2])
+                
+                
+            
+        
